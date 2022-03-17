b797a54ae3e86e4b6d29b1b47a839a5761ac9c66
prashantchavan <prashantchavan312199@gmail.com>
2022-03-17 16:53:30 +0530

    Updates

---
+++ precache-manifest.9a0433192fc58aaf92142051a1321ac4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e482bf9bec81a8936e3",
+    "url": "/Tik-Tok-clone/static/css/main.5874b7de.chunk.css"
+  },
+  {
+    "revision": "4e482bf9bec81a8936e3",
+    "url": "/Tik-Tok-clone/static/js/main.4e482bf9.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/Tik-Tok-clone/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "3a2e0e0b763789a1abbb",
+    "url": "/Tik-Tok-clone/static/js/2.3a2e0e0b.chunk.js"
+  },
+  {
+    "revision": "cd0dae942da033acdb1b",
+    "url": "/Tik-Tok-clone/static/js/runtime~main.cd0dae94.js"
+  },
+  {
+    "revision": "471acf072bea6b22ab21c05cf2baebc4",
+    "url": "/Tik-Tok-clone/index.html"
+  }
+];
